2c40152c2fb12008c0464bb4422fd6e18e13e5c7
Santiago Jofré Montano <santiago.jofre@alumnos.uach.cl>
2024-05-09 18:36:17 -0400

    titulo

--- infodle/src/app/page.js
+++ infodle/src/app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return(
     <center>
-      <h1 className="caja">Mi primera pagina en linea</h1>
+      <h1>INFODLE</h1>
+      <div className="caja">
+        Mi primera pagina en linea
+      </div>
     </center>
   );
 }
