6fbba9a53cc035f5d15b559acb6339df459de768
Bastian Gajardo <bastian.gajardo01@alumnos.uach.cl>
2024-06-07 16:54:50 -0400

    actualizar variables a español

--- infodle/src/components/imagenProfesor.js
+++ infodle/src/components/imagenProfesor.js
@@ -1,8 +1,8 @@
 // src/components/ProfesorImage.js
 import React from 'react';
 
-const ProfesorImage = ({ src, alt }) => (
+const ImagenProfesor = ({ src, alt }) => (
   <img src={src} alt={alt} className="w-24 h-24.1 rounded-full object-cover m-4" />
 );
 
-export default ProfesorImage;
+export default ImagenProfesor;
