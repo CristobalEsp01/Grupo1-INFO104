97330e44c0410298e95d13ee968431cbbf20abf7
Bastian Gajardo <bastian.gajardo01@alumnos.uach.cl>
2024-06-07 16:49:53 -0400

    modularizacion de profesorCard

--- infodle/src/components/profesorCard.js
+++ infodle/src/components/profesorCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import AtributosCard from './atributosCard';
+import ImagenProfesor from './imagenProfesor';
 
 const ProfesorCard = ({ profesor, profesorDelDia }) => {
   const getColor = (atributo) => {
@@ -12,17 +14,15 @@ const ProfesorCard = ({ profesor, profesorDelDia }) => {
     }
   };
 
-  const cardStyle = (atributo) => `flex justify-center items-center ${getColor(atributo)} text-white font-bold text-lg h-20 w-32 m-2 rounded-md text-center 4`;
-
   return (
     <div className="flex justify-center items-center space-x-4 p-0.5 rounded-lg">
-      <img src={profesor.foto} alt={`${profesor.nombre} foto`} className="w-24 h-24.1 rounded-full object-cover m-4" />      
-      <div className={cardStyle('genero')}>{profesor.genero}</div>
-      <div className={cardStyle('gradAcademico')}>{profesor.gradAcademico}</div>
-      <div className={cardStyle('carrera')}>{profesor.carrera}</div>
-      <div className={cardStyle('universidad')}>{profesor.universidad}</div>
-      <div className={cardStyle('egreso')}>{profesor.egreso}</div>
-      <div className={cardStyle('asignaturas')}>{profesor.asignaturas.join(', ')}</div>
+      <ImagenProfesor src={profesor.foto} alt={`${profesor.nombre} foto`} />
+      <AtributosCard atributo="genero" valor={profesor.genero} color={getColor('genero')} />
+      <AtributosCard atributo="gradAcademico" valor={profesor.gradAcademico} color={getColor('gradAcademico')} />
+      <AtributosCard atributo="carrera" valor={profesor.carrera} color={getColor('carrera')} />
+      <AtributosCard atributo="universidad" valor={profesor.universidad} color={getColor('universidad')} />
+      <AtributosCard atributo="egreso" valor={profesor.egreso} color={getColor('egreso')} />
+      <AtributosCard atributo="asignaturas" valor={profesor.asignaturas.join(', ')} color={getColor('asignaturas')} />
     </div>
   );
 };
