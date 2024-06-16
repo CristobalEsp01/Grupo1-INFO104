53b0860cea730caf020d0730044457ea57a6e7df
Bastian Gajardo <bastian.gajardo01@alumnos.uach.cl>
2024-06-16 17:42:14 -0400

    actualizacion para elegir profe aleatorio
    se cambia a un hash

--- infodle/src/app/page.js
+++ infodle/src/app/page.js
@@ -20,18 +20,42 @@ export default function Home() {
   }, []);
 
   const normalizeString = (str) => {
-    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    return str.normalize("NFD").replace(/[\\u0300-\\u036f]/g, "");
+  };
+
+  const hashCode = (str) => {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charCodeAt(i);
+      hash = (hash << 5) - hash + char;
+      hash |= 0; // Convierte a 32-bit int 
+    }
+    return hash;
   };
 
   const seleccionarProfesorDelDia = () => {
     const hoy = new Date().toISOString().split('T')[0];
     const profesorAlmacenado = JSON.parse(localStorage.getItem('profesorDelDia'));
+    const historialProfesores = JSON.parse(localStorage.getItem('historialProfesores')) || [];
 
     if (profesorAlmacenado && profesorAlmacenado.fecha === hoy) {
       setProfesorDelDia(profesorAlmacenado.profesor);
     } else {
-      const profesorAleatorio = profesores[Math.floor(Math.random() * profesores.length)];
+      let profesorAleatorio;
+      const maxAttempts = 10;
+      let attempts = 0;
+
+      do {
+        const hash = hashCode(hoy + attempts);
+        const index = Math.abs(hash) % profesores.length;
+        profesorAleatorio = profesores[index];
+        attempts++;
+      } while (historialProfesores.includes(profesorAleatorio.nombre) && attempts < maxAttempts);
+
       setProfesorDelDia(profesorAleatorio);
+
+      const nuevoHistorial = [...historialProfesores, profesorAleatorio.nombre].slice(-5);
+      localStorage.setItem('historialProfesores', JSON.stringify(nuevoHistorial));
       localStorage.setItem('profesorDelDia', JSON.stringify({ profesor: profesorAleatorio, fecha: hoy }));
     }
   };
@@ -62,13 +86,11 @@ export default function Home() {
   const handleOpenModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
 
-  
-
   return (
     <>
-    <div className={`fixed inset-0 ${showModal ? 'bg-gray-900 bg-opacity-10 backdrop-blur-sm' : ''} z-10`}>
+      <div className={`fixed inset-0 ${showModal ? 'bg-gray-900 bg-opacity-10 backdrop-blur-sm' : ''} z-10`}>
         <ModalInstrucciones showModal={showModal} handleCloseModal={handleCloseModal} />
-    </div>
+      </div>
       <div className={`p-6 ${showModal ? 'opacity-50' : ''}`}>
         <TituloInfodle/>
         <BarraBusqueda
