be8fd2784404c391446e9df67424a2c0a89ae142
MaxArayaE <max.araya.e@gmail.com>
2024-06-23 23:19:11 -0400

    Agregado paso de id a nombre y cambios a json ramos, además de estilizar para visualización del nombre

--- infodle/src/components/atributosElemento.js
+++ infodle/src/components/atributosElemento.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const ElementoAtributos = ({valor, color }) => (
-  <div className={`flex justify-center items-center ${color} text-white font-bold text-lg h-20 w-32 m-2 rounded-md text-center border-solid border-2 border-slate-950`}>
+const ElementoAtributos = ({
+  valor,
+  color,
+  tamanoTexto = "text-lg",
+  alto = "h-20",
+}) => (
+  <div
+    className={`flex justify-center items-center ${color} text-white font-bold ${tamanoTexto} ${alto} w-32 m-2 rounded-md text-center border-solid border-2 border-slate-950`}
+  >
     {valor}
   </div>
 );
--- infodle/src/components/profesorCard.js
+++ infodle/src/components/profesorCard.js
@@ -1,35 +1,76 @@
-import React from 'react';
-import AtributosElemento from './atributosElemento';
-import ImagenProfesor from './imagenProfesor';
+import React from "react";
+import AtributosElemento from "./atributosElemento";
+import ImagenProfesor from "./imagenProfesor";
+import ramos from "../data/ramos.json";
 
 const ProfesorCard = ({ profesor, profesorDelDia }) => {
   const getColor = (atributo) => {
     if (profesor[atributo] === profesorDelDia[atributo]) {
-      return 'bg-green-500';
-    } else if (atributo === 'asignaturas') {
-      const coincidencias = profesor[atributo].filter(asignatura => profesorDelDia[atributo].includes(asignatura));
-      const igualdadArreglos = profesor[atributo].length === profesorDelDia[atributo].length && profesor[atributo].every(asignatura => profesorDelDia[atributo].includes(asignatura));
-       if(igualdadArreglos) {
-          return 'bg-green-500';
-        } else if (coincidencias.length > 0) {
-          return 'bg-orange-500';
-        } else {
-          return 'bg-red-500';
-        }
+      return "bg-green-500";
+    } else if (atributo === "asignaturas") {
+      const coincidencias = profesor[atributo].filter((asignatura) =>
+        profesorDelDia[atributo].includes(asignatura)
+      );
+      const igualdadArreglos =
+        profesor[atributo].length === profesorDelDia[atributo].length &&
+        profesor[atributo].every((asignatura) =>
+          profesorDelDia[atributo].includes(asignatura)
+        );
+      if (igualdadArreglos) {
+        return "bg-green-500";
+      } else if (coincidencias.length > 0) {
+        return "bg-orange-500";
+      } else {
+        return "bg-red-500";
+      }
     } else {
-      return 'bg-red-500';
+      return "bg-red-500";
     }
+  };
+
+  const nombreRamo = (asignaturas) => {
+    const nomAsignaturas = ramos
+      .filter((ramos) => asignaturas.includes(ramos.id))
+      .map((ramos) => ramos.name);
+
+    return nomAsignaturas.join(" - ");
   };
 
   return (
     <div className="flex justify-center items-center space-x-4 p-0.5 rounded-lg ">
       <ImagenProfesor src={profesor.foto} alt={`${profesor.nombre} foto`} />
-      <AtributosElemento atributo="genero" valor={profesor.genero} color={getColor('genero')} />
-      <AtributosElemento atributo="gradAcademico" valor={profesor.gradAcademico} color={getColor('gradAcademico')} />
-      <AtributosElemento atributo="carrera" valor={profesor.carrera} color={getColor('carrera')} />
-      <AtributosElemento atributo="universidad" valor={profesor.universidad} color={getColor('universidad')} />
-      <AtributosElemento atributo="egreso" valor={profesor.egreso} color={getColor('egreso')} />
-      <AtributosElemento atributo="asignaturas" valor={profesor.asignaturas.join(', ')} color={getColor('asignaturas')} />
+      <AtributosElemento
+        atributo="genero"
+        valor={profesor.genero}
+        color={getColor("genero")}
+      />
+      <AtributosElemento
+        atributo="gradAcademico"
+        valor={profesor.gradAcademico}
+        color={getColor("gradAcademico")}
+      />
+      <AtributosElemento
+        atributo="carrera"
+        valor={profesor.carrera}
+        color={getColor("carrera")}
+      />
+      <AtributosElemento
+        atributo="universidad"
+        valor={profesor.universidad}
+        color={getColor("universidad")}
+      />
+      <AtributosElemento
+        atributo="egreso"
+        valor={profesor.egreso}
+        color={getColor("egreso")}
+      />
+      <AtributosElemento
+        atributo="asignaturas"
+        valor={nombreRamo(profesor.asignaturas)}
+        color={getColor("asignaturas")}
+        tamanoTexto="text-xxs"
+        alto={profesor.asignaturas.length > 2 ? "h-30" : "h-20"}
+      />
     </div>
   );
 };
