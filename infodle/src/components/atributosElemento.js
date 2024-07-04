c53b217d37c79a704f9941262ec0152d1a4ace3d
Bastian Gajardo <120329341+darkanthem21@users.noreply.github.com>
2024-07-04 17:45:00 -0400

    cambio bordes

--- infodle/src/components/atributosElemento.js
+++ infodle/src/components/atributosElemento.js
@@ -7,7 +7,7 @@ const ElementoAtributos = ({
   alto = "h-20",
 }) => (
   <div
-    className={`flex justify-center items-center ${color} text-white font-bold ${tamanoTexto} ${alto} w-32 m-2 rounded-md text-center border-solid border-2 border-slate-950`}
+    className={`flex justify-center items-center ${color} text-white font-bold ${tamanoTexto} ${alto} w-32 m-2 rounded-md text-center`}
   >
     {valor}
   </div>
--- infodle/src/components/profesorGrid.js
+++ infodle/src/components/profesorGrid.js
@@ -4,13 +4,13 @@ import ProfesorCard from './profesorCard';
 const ProfessorGrid = ({ adivinanzas, profesorDelDia }) => (
   <div className="mt-10 text-center z-15">
     <div className="flex justify-center space-x-4 mb-4">
-      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center border-solid border-2 border-slate-950">Profesor</div>
-      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center border-solid border-2 border-slate-950">Género</div>
-      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center border-solid border-2 border-slate-950">Grado Académico</div>
-      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center border-solid border-2 border-slate-950">Carrera</div>
-      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center border-solid border-2 border-slate-950">Universidad</div>
-      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center border-solid border-2 border-slate-950">Egreso</div>
-      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center border-solid border-2 border-slate-950">Asignaturas</div>
+      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center">Profesor</div>
+      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center">Género</div>
+      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center">Grado Académico</div>
+      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center">Carrera</div>
+      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center">Universidad</div>
+      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center">Egreso</div>
+      <div className="bg-gray-200 p-2 rounded-md w-32 h-20 flex justify-center items-center">Asignaturas</div>
     </div>
     {adivinanzas.map((profesor, index) => (
       <ProfesorCard key={index} profesor={profesor} profesorDelDia={profesorDelDia} />
